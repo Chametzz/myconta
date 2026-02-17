aefd970bfc939aca98ea36d3c9a42b05a7ace200
david22 <davidcastrejonrojas@gmail.com>
2026-02-17 15:36:10 -0600

    POO para MyAccount

--- views/my_account.py
+++ views/my_account.py
@@ -1,71 +1,63 @@
 import tkinter as tk
 from tkinter import messagebox
 
-# Crear ventana
-ventana = tk.Tk()
-ventana.title("Configuración")
-ventana.geometry("360x500")
-ventana.config(bg="#eaeaea")#cambia el color del fondoo
+class MyAccount(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.create_option("Correo Electrónico")
+        self.create_option("Mis cuentas")
 
-# Funciones
-def accion(texto):
-    messagebox.showinfo("Opción", f"Seleccionaste: {texto}") # muestra una ventanita con el mensaje 
+        self.create_section("Descargas")
+        self.create_option("Historial de Gastos e ingresos")
 
-def crear_seccion(titulo):
-    label = tk.Label(
-        ventana,
-        text=titulo,
-        bg="#eaeaea",
-        fg="blue",
-        font=("Arial", 11, "bold")
-    )
-    label.pack(anchor="w", padx=20, pady=(15,5))
- ##pone de color los subtitulos  y lo alinia a la izquierda 
+        self.create_section("Cuenta")
+        self.create_option("Cerrar sesión")
+        self.create_option("Cambiar de cuenta")
+        self.create_option("Eliminar cuenta")
 
-def crear_opcion(texto):
-    frame = tk.Frame(ventana, bg="white")
-    frame.pack(fill="x", padx=15, pady=2)## se crea un contenedor para que le boton y las flechas esten juntos 
 
-    boton = tk.Button(
-        frame,
-        text=texto,
-        anchor="w",
-        bg="white",
-        relief="flat",
-        padx=15,
-        pady=12,
-        font=("Arial", 11),
-        command=lambda: accion(texto)#ejecuta el boton cuando se le da click 
-    )
-    boton.pack(side="left", fill="x", expand=True)
+    def create_section(self, text):
+        label = tk.Label(
+            self,
+            text=text,
+            bg="#eaeaea",
+            fg="blue",
+            font=("Arial", 11, "bold")
+        )
+        label.pack(anchor="w", padx=20, pady=(15,5))
+    
+    def create_option(self, text):
+        frame = tk.Frame(self, bg="white")
+        frame.pack(fill="x", padx=15, pady=2)## se crea un contenedor para que le boton y las flechas esten juntos 
 
-    flecha = tk.Label(
-        frame,
-        text=">",
-        bg="white",
-        fg="gray",
-        font=("Arial", 12, "bold")
-    ) 
-    flecha.pack(side="right", padx=10)
-#solamente crea las flechas en los botones 
+        boton = tk.Button(
+            frame,
+            text=text,
+            anchor="w",
+            bg="white",
+            relief="flat",
+            padx=15,
+            pady=12,
+            font=("Arial", 11),
+            command=lambda: self.action(text)#ejecuta el boton cuando se le da click 
+        )
+        boton.pack(side="left", fill="x", expand=True)
 
-titulo = tk.Label( #crea todo en la pantalla , toda la interfaz grafica se crea aqui 
-    ventana,
-    text="Configuración",
-    bg="#eaeaea",
-    font=("Arial", 16, "bold")
-)
-titulo.pack(pady=20)
-
-crear_opcion("Correo Electrónico")
-crear_opcion("Mis cuentas")
-
-crear_seccion("Descargas")
-crear_opcion("Historial de Gastos e ingresos")
-
-crear_seccion("Cuenta")
-crear_opcion("Cerrar sesión")
-crear_opcion("Cambiar de cuenta")
-crear_opcion("Eliminar cuenta")
-
-ventana.mainloop()
+        flecha = tk.Label(
+            frame,
+            text=">",
+            bg="white",
+            fg="gray",
+            font=("Arial", 12, "bold")
+        ) 
+        flecha.pack(side="right", padx=10)
+        
+    def action(self, text):
+        messagebox.showinfo("Opción", f"Seleccionaste: {text}") # muestra una ventanita con el mensaje 
+    
+#Test
+if __name__ == "__main__":
+    root = tk.Tk()
+    my_account = MyAccount(root)
+    my_account.pack(fill="both", expand=True)
+    root.mainloop()
